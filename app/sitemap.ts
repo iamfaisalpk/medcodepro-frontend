772e3c88eb44996d0d7bbed92d8552a6475e75f9
iamfaisalpk <iamfaizy123@gmail.com>
2026-02-17 12:04:28 +0530

    some sitemap changes

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -10,5 +10,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'yearly',
       priority: 1,
     },
+    {
+      url: `${baseUrl}/login`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/register`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
   ];
 }
